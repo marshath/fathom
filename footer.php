076f58f1e472ec2c5cf1e8099d0abf2ae830e8de
Theodore Marshall <ted@tedm.net>
2019-08-30 07:07:51 -0700

    Add basic form css

--- footer.php
+++ footer.php
@@ -1,17 +1,13 @@
 			<footer class="footer-wrap" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-				<div id="inner-footer" class="inner-wrap">
+				<div id="inner-footer" class="footer-inner-wrap">
 
 					<?php if ( is_active_sidebar( 'footer1' ) ) : ?>
-	
 						<?php dynamic_sidebar( 'footer1' ); ?>
-	
 					<?php else : ?>
-	
 						<?php /* This content shows up if there are no widgets defined in the backend. */ ?>
 						<div class="no-widgets">
 							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
 						</div>
-	
 					<?php endif; ?>
 	
 						<nav class="nav-footer" role="navigation">
